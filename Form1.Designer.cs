59f7439fe7d09c7eecdbb557f8d8eee71e47a6f2
duy.ho <hongocduyyu@gmail.com>
2023-05-05 20:31:50 +0700

    Add logic for question and asnswer and update UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,120 +34,96 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.Pic1 = new System.Windows.Forms.PictureBox();
             this.AnsA = new System.Windows.Forms.Label();
             this.AnsB = new System.Windows.Forms.Label();
             this.AnsC = new System.Windows.Forms.Label();
             this.AnsD = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtQuestion
             // 
             this.txtQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.txtQuestion.Enabled = false;
-            this.txtQuestion.Font = new System.Drawing.Font("PMingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuestion.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtQuestion.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtQuestion.Location = new System.Drawing.Point(602, 43);
+            this.txtQuestion.Location = new System.Drawing.Point(352, 78);
             this.txtQuestion.Name = "txtQuestion";
             this.txtQuestion.ReadOnly = true;
-            this.txtQuestion.Size = new System.Drawing.Size(277, 33);
+            this.txtQuestion.Size = new System.Drawing.Size(270, 35);
             this.txtQuestion.TabIndex = 0;
             this.txtQuestion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.SeaShell;
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.button1.Location = new System.Drawing.Point(167, 208);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(166, 223);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 79);
+            this.button1.Size = new System.Drawing.Size(203, 58);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.SeaShell;
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
-            this.button2.ForeColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(682, 208);
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(677, 223);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(203, 79);
+            this.button2.Size = new System.Drawing.Size(203, 58);
             this.button2.TabIndex = 2;
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.SeaShell;
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
-            this.button3.ForeColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(171, 318);
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Location = new System.Drawing.Point(166, 333);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(203, 79);
+            this.button3.Size = new System.Drawing.Size(203, 58);
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.SeaShell;
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackColor = System.Drawing.Color.White;
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Bold);
-            this.button4.ForeColor = System.Drawing.Color.Black;
-            this.button4.Location = new System.Drawing.Point(682, 318);
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.Location = new System.Drawing.Point(677, 333);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(203, 79);
+            this.button4.Size = new System.Drawing.Size(203, 58);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.Pic1);
-            this.panel1.Location = new System.Drawing.Point(8, 6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(533, 179);
-            this.panel1.TabIndex = 5;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // Pic1
-            // 
-            this.Pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Pic1.Image = ((System.Drawing.Image)(resources.GetObject("Pic1.Image")));
-            this.Pic1.Location = new System.Drawing.Point(3, 3);
-            this.Pic1.Name = "Pic1";
-            this.Pic1.Size = new System.Drawing.Size(527, 178);
-            this.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Pic1.TabIndex = 0;
-            this.Pic1.TabStop = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // AnsA
             // 
             this.AnsA.AutoSize = true;
-            this.AnsA.Font = new System.Drawing.Font("Engravers MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AnsA.ForeColor = System.Drawing.Color.Gold;
-            this.AnsA.Image = ((System.Drawing.Image)(resources.GetObject("AnsA.Image")));
+            this.AnsA.BackColor = System.Drawing.Color.Transparent;
+            this.AnsA.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsA.ForeColor = System.Drawing.Color.DarkGreen;
             this.AnsA.Location = new System.Drawing.Point(122, 229);
             this.AnsA.Name = "AnsA";
-            this.AnsA.Size = new System.Drawing.Size(39, 31);
+            this.AnsA.Size = new System.Drawing.Size(36, 37);
             this.AnsA.TabIndex = 6;
             this.AnsA.Text = "A";
-            this.AnsA.Click += new System.EventHandler(this.label1_Click);
             // 
             // AnsB
             // 
+            this.AnsB.BackColor = System.Drawing.Color.Transparent;
             this.AnsB.Cursor = System.Windows.Forms.Cursors.Default;
-            this.AnsB.Font = new System.Drawing.Font("Engravers MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AnsB.ForeColor = System.Drawing.Color.Gold;
-            this.AnsB.Image = ((System.Drawing.Image)(resources.GetObject("AnsB.Image")));
+            this.AnsB.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsB.ForeColor = System.Drawing.Color.DarkGreen;
             this.AnsB.Location = new System.Drawing.Point(122, 340);
             this.AnsB.Name = "AnsB";
             this.AnsB.Size = new System.Drawing.Size(39, 31);
@@ -157,26 +133,37 @@
             // AnsC
             // 
             this.AnsC.AutoSize = true;
-            this.AnsC.Font = new System.Drawing.Font("Engravers MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AnsC.ForeColor = System.Drawing.Color.Gold;
-            this.AnsC.Image = ((System.Drawing.Image)(resources.GetObject("AnsC.Image")));
+            this.AnsC.BackColor = System.Drawing.Color.Transparent;
+            this.AnsC.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsC.ForeColor = System.Drawing.Color.DarkGreen;
             this.AnsC.Location = new System.Drawing.Point(637, 229);
             this.AnsC.Name = "AnsC";
-            this.AnsC.Size = new System.Drawing.Size(38, 31);
+            this.AnsC.Size = new System.Drawing.Size(34, 37);
             this.AnsC.TabIndex = 8;
             this.AnsC.Text = "C";
             // 
             // AnsD
             // 
             this.AnsD.AutoSize = true;
-            this.AnsD.Font = new System.Drawing.Font("Engravers MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AnsD.ForeColor = System.Drawing.Color.Gold;
-            this.AnsD.Image = ((System.Drawing.Image)(resources.GetObject("AnsD.Image")));
+            this.AnsD.BackColor = System.Drawing.Color.Transparent;
+            this.AnsD.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsD.ForeColor = System.Drawing.Color.DarkGreen;
             this.AnsD.Location = new System.Drawing.Point(637, 340);
             this.AnsD.Name = "AnsD";
-            this.AnsD.Size = new System.Drawing.Size(42, 31);
+            this.AnsD.Size = new System.Drawing.Size(37, 37);
             this.AnsD.TabIndex = 9;
             this.AnsD.Text = "D";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(591, 39);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Who is smarter than 5th grade student";
             // 
             // QuizFrm
             // 
@@ -184,11 +171,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(987, 422);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.AnsD);
             this.Controls.Add(this.AnsC);
             this.Controls.Add(this.AnsB);
             this.Controls.Add(this.AnsA);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -197,8 +184,6 @@
             this.Name = "QuizFrm";
             this.Text = "Quiz Game";
             this.Load += new System.EventHandler(this.QuizFrm_Load);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,12 +196,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox Pic1;
         private System.Windows.Forms.Label AnsA;
         private System.Windows.Forms.Label AnsB;
         private System.Windows.Forms.Label AnsC;
         private System.Windows.Forms.Label AnsD;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Calculate_Quiz
 {
     public partial class QuizFrm : Form
     {
+        private int limit = 500;
+        /* 
+         * Init question & answer
+         * a & b are 2 numbers
+         * op is the operator (0 : + | 1 : - | 2 : * | 3 : /)
+         * result is the result of the calculation
+         */
+        private int a, b, op, result;
+        // Result position
+        private int resultPos;
+
         public QuizFrm()
         {
             InitializeComponent();
+            randomQuestionAndResult();
+            randomAnswers();
+        }
 
+        private void randomQuestionAndResult()
+        {
+            // Random 0 <= a < limit, 0 < b <= limit
+            Random rnd = new Random();
+            a = rnd.Next(limit);
+            b = rnd.Next(limit) + 1;
+            op = rnd.Next(4);
+            switch (op)
+            {
+                case 0:
+                    {
+                        result = a + b;
+                        this.txtQuestion.Text = a + " + " + b + " = ?";
+                        break;
+                    }
+                case 1:
+                    {
+                        result = a - b;
+                        this.txtQuestion.Text = a + " - " + b + " = ?";
+                        break;
+                    }
+                case 2:
+                    {
+                        result = a * b;
+                        this.txtQuestion.Text = a + " x " + b + " = ?";
+                        break;
+                    }
+                case 3:
+                    {
+                        result = a / b;
+                        this.txtQuestion.Text = a + " / " + b + " = ?";
+                        break;
+                    }
+            }
+        }
+
+        private void randomAnswers()
+        {
+            // Random result position
+            Random rnd = new Random();
+            resultPos = rnd.Next(4);
+            // Random answer
+            this.button1.Text = rnd.Next(limit * 5).ToString();
+            this.button2.Text = rnd.Next(limit * 5).ToString();
+            this.button3.Text = rnd.Next(limit * 5).ToString();
+            this.button4.Text = rnd.Next(limit * 5).ToString();
+            // Put correct result into correct position answer
+            if (resultPos == 0) this.button1.Text = result.ToString();
+            if (resultPos == 1) this.button2.Text = result.ToString();
+            if (resultPos == 2) this.button3.Text = result.ToString();
+            if (resultPos == 3) this.button4.Text = result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (resultPos == 0) popupCorrectMessageBox();
+            else popupWrongMessageBox();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (resultPos == 1) popupCorrectMessageBox();
+            else popupWrongMessageBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (resultPos == 2) popupCorrectMessageBox();
+            else popupWrongMessageBox();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (resultPos == 3) popupCorrectMessageBox();
+            else popupWrongMessageBox();
         }
 
         private void popupCorrectMessageBox()
@@ -27,18 +108,7 @@ namespace Calculate_Quiz
         {
             MessageBox.Show("Wrong!!!", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
         private void QuizFrm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
         {
 
         }
